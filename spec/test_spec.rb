7480f54364140579c2980676c84e635ca0b6f16f
Yan Ramanovich <Yan_Ramanovich@epam.com>
2021-04-07 20:14:05 +0300

    Added a few tests

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,6 +1,25 @@
 describe 'Visit the Webpage' do
-  it 'view the index page' do
+  it 'view the main page' do
     visit('https://www.saucedemo.com/')
-    expect(page).to have_content('Accepted usernames are:')
+    expect(page).to have_field('Username')
   end
+
+  it 'view the loging with wrong credentials' do
+    visit('https://www.saucedemo.com')
+    fill_in 'Username', with: 'Name'
+    fill_in 'Password', with: 'Password'
+    click_button 'Login'
+    expect(page).to have_selector(:xpath, '//*[@id="login_button_container"]/div/form/div[3]')
+  end
+
+  it 'view the loging with correct credentials' do
+    visit('https://www.saucedemo.com')
+    fill_in 'Username', with: 'standard_user'
+    fill_in 'Password', with: 'secret_sauce'
+    click_button 'Login'
+    expect(page).to have_content('PRODUCTS')
+  end
+
+
+
 end
